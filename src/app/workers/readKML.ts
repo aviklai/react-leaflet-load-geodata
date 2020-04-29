5f6ed2c63520f4891856e39804245b9b869a226b
Avi <aviklaiman@hotmail.com>
2020-04-29 13:18:27 +0300

    Added missing semicolons

--- src/app/workers/readKML.ts
+++ src/app/workers/readKML.ts
@@ -1,6 +1,6 @@
-import * as dom from 'xmldom'
+import * as dom from 'xmldom';
 // @ts-ignore
-import { kml } from '@tmcw/togeojson/dist/togeojson.es.js'
+import { kml } from '@tmcw/togeojson/dist/togeojson.es.js';
 import { expose } from 'threads/worker';
 import { readFileAsync } from 'app/utils';
 
